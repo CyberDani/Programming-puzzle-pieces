8db851537d5ce05cd469c4d83f7e3f654fce5ed2
Feldman Daniel <dani.feldman111@gmail.com>
2022-04-09 15:38:54 +0300

    Add function to download file from given URL
    Will use this function to download my db file.

--- webPage/generator/modules/webReq.py
+++ webPage/generator/modules/webReq.py
@@ -6,3 +6,13 @@ def getstatusCodeEncodingAndHtmlFromUrl(url):
   checks.checkIfString(url, 9, 500)
   r = requests.get(url)
   return r.status_code, r.encoding, r.text
+
+def downloadFromUrlToFileIfStatusIs200(url, filePath):
+  checks.checkIfString(url, 9, 500)
+  checks.checkIfString(filePath, 3, 300)
+  r = requests.get(url, stream=True)
+  if r.status_code != 200:
+    raise Exception('Status Code {0} returned, download prevented!'.format(r.status_code))
+  with open(filePath, 'wb') as fd:
+    for chunk in r.iter_content(chunk_size=512000): # 500 kb chunks
+      fd.write(chunk)
--- webPage/generator/unitTests/webReq_test.py
+++ webPage/generator/unitTests/webReq_test.py
@@ -1,8 +1,10 @@
+import os
 import sys
 import unittest
 
 sys.path.append('..')
 from modules import webReq
+from modules import filerw
 
 class WebReqTests(unittest.TestCase):
 
@@ -33,3 +35,40 @@ class WebReqTests(unittest.TestCase):
     self.assertEqual(statusCode, 404)
     self.assertEqual(encoding, "utf-8")
     self.assertTrue(len(html) > 300)
+
+  def test_downloadFromUrlToFile_nonSense(self):
+    file = open("./unitTests/temp/test.txt", "wb")
+    with self.assertRaises(Exception):
+      webReq.downloadFromUrlToFileIfStatusIs200("io", "./unitTests/temp/logo.binary")
+    with self.assertRaises(Exception):
+      webReq.downloadFromUrlToFileIfStatusIs200("https://cyberdani.github.io/Programming-puzzle-pieces/webPage/images/Logo_text.png", file)
+    with self.assertRaises(Exception):
+      webReq.downloadFromUrlToFileIfStatusIs200("https://cyberdani.github.io/Programming-puzzle-pieces/webPage/images/Logo_text.png", "")
+    with self.assertRaises(Exception):
+      webReq.downloadFromUrlToFileIfStatusIs200("https://cyberdani.github.io/Programming-puzzle-pieces/webPage/images/Logo_text.png", None)
+    with self.assertRaises(Exception):
+      webReq.downloadFromUrlToFileIfStatusIs200(None, "./unitTests/temp/logo.binary")
+    with self.assertRaises(Exception):
+      webReq.downloadFromUrlToFileIfStatusIs200(None, None)
+    self.assertFalse(filerw.fileExists("./unitTests/temp/logo.binary"))
+
+  def test_downloadFromUrlToFile_incorrectUrl(self):
+    with self.assertRaises(Exception):
+      statusCode, encoding, html = webReq.downloadFromUrlToFileIfStatusIs200("http://www.asdsasdsadasdas.qweqwe.com", "./unitTests/temp/download.binary")
+    self.assertFalse(filerw.fileExists("./unitTests/temp/download.binary"))
+
+  def test_downloadFromUrlToFile_404(self):
+    with self.assertRaises(Exception):
+      webReq.downloadFromUrlToFileIfStatusIs200("https://www.youtube.com/q1w2e3r4t5", "./unitTests/temp/download.binary")
+
+  def test_downloadFromUrlToFile_correctUrl200(self):
+    webReq.downloadFromUrlToFileIfStatusIs200("https://cyberdani.github.io/Programming-puzzle-pieces/webPage/images/Logo_text.png", "./unitTests/temp/download.png")
+    self.assertTrue(filerw.fileExists("./unitTests/temp/download.png"))
+    size1 = os.path.getsize("./unitTests/temp/download.png") / 1024
+    self.assertTrue(size1 > 15)
+    self.assertTrue(size1 < 150)
+    webReq.downloadFromUrlToFileIfStatusIs200("https://cyberdani.github.io/Programming-puzzle-pieces/webPage/images/Logo_text.png", "./unitTests/temp/download.png")
+    self.assertTrue(filerw.fileExists("./unitTests/temp/download.png"))
+    size2 = os.path.getsize("./unitTests/temp/download.png") / 1024
+    self.assertEqual(size1, size2)
+    os.remove("./unitTests/temp/download.png")
