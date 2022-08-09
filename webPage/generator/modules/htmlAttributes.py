03d44f1fcd293c82c258e080628c0796bab1a8b4
Feldman Daniel <dani.feldman111@gmail.com>
2022-08-09 03:18:28 +0300

    Add function getNextHtmlAttribute
    Working but it is not clean code yet.

--- webPage/generator/modules/htmlAttributes.py
+++ webPage/generator/modules/htmlAttributes.py
@@ -60,7 +60,7 @@ def extractDifferentWhiteSpaceSeparatedValuesFromHtmlAttributesByKey(htmlAttribu
     return result
   # TODO getFirstWhiteSpaceCharIdx
   # TODO getFirstCharIdx(string, skip=[WhiteSpace, ","], find=[AnyChar, "="])
-  attrValues = htmlAttributes[startingQuoteIdx + 1 : closingQuotePos]
+  attrValues = htmlAttributes[startingQuoteIdx + 1: closingQuotePos]
   values = attrValues.split()
   for value in values:
     if value not in result:
@@ -68,51 +68,89 @@ def extractDifferentWhiteSpaceSeparatedValuesFromHtmlAttributesByKey(htmlAttribu
   return result
 
 # TODO this function is too long, make it shorter
+def getNextHtmlAttribute(attributesString, startIdx):
+  """ Raises exception if <startIdx> is not valid. This means that <attributesString> cannot be an empty string as
+there is no first index. \n
+<attributesString> can be considered corrupt only within the context of the first attribute \n
+<startIdx>-1 is not accessed for full word check
+\n Return values:
+* <attributeName>, <attributeValue> : **None** if no attribute was found or <attributesString> is corrupt
+* <attrStartIdx>, <attrEndIdx> : inclusive, **-1** if no attribute was found or attributesString is corrupt"""
+  checks.checkIfString(attributesString, 0, 1000)
+  checks.checkIntIsBetween(startIdx, 0, len(attributesString) - 1)
+  attrStartIdx = -1
+  currentIdx = startIdx
+  currentAttribute = ""
+  lastEndIdx = -2
+  while currentIdx < len(attributesString):
+    currentChar = attributesString[currentIdx]
+    nextChar = None
+    if currentIdx + 1 != len(attributesString):
+      nextChar = attributesString[currentIdx + 1]
+    if not currentChar.isspace() and currentChar != "=":
+      if currentChar == "'" or currentChar == "\"":
+        return None, None, -1, -1
+      if not currentAttribute:
+        attrStartIdx = currentIdx
+      currentAttribute += currentChar
+      currentIdx += 1
+      continue
+    if currentAttribute and currentChar.isspace():
+      if nextChar is None or (not nextChar.isspace() and nextChar != "="):
+        if nextChar != "'" and nextChar != "\"":
+          if lastEndIdx > 0:
+            return currentAttribute, None, attrStartIdx, lastEndIdx
+          else:
+            return currentAttribute, None, attrStartIdx, currentIdx - 1
+        else:
+          return None, None, -1, -1
+      if currentIdx > startIdx and not attributesString[currentIdx - 1].isspace():
+        lastEndIdx = currentIdx - 1
+      currentIdx += 1
+      continue
+    elif currentAttribute and currentChar == "=":
+      if currentIdx == len(attributesString) - 1:
+        return None, None, -1, -1
+      nextNonSpaceCharIdx = stringUtil.getFirstNonWhiteSpaceCharIdx(attributesString, currentIdx + 1,
+                                                                    len(attributesString))
+      if nextNonSpaceCharIdx == -1:
+        return None, None, -1, -1
+      nextNonSpaceChar = attributesString[nextNonSpaceCharIdx]
+      if nextNonSpaceChar != "\"" and nextNonSpaceChar != "'":
+        return None, None, -1, -1
+      quoteCharUsed = nextNonSpaceChar
+      closingQuoteIdx = attributesString.find(quoteCharUsed, nextNonSpaceCharIdx + 1)
+      if closingQuoteIdx == -1:
+        return None, None, -1, -1
+      return currentAttribute, attributesString[nextNonSpaceCharIdx+1:closingQuoteIdx], attrStartIdx, closingQuoteIdx
+    elif not currentAttribute and currentChar.isspace():
+      currentIdx += 1
+      continue
+    elif not currentAttribute and currentChar == "=":
+      return None, None, -1, -1
+  if currentAttribute:
+    if lastEndIdx > 0:
+      return currentAttribute, None, attrStartIdx, lastEndIdx
+    else:
+      return currentAttribute, None, attrStartIdx, len(attributesString) - 1
+  return None, None, -1, -1
+
 def getListOfHtmlAttributes(attributesString):
   """Returns empty list if attribute not found, for empty string and if **<attributesString>** is corrupt \n
      Only the first declaration is taken (if there are multiple) as stated by the standard:
      https://stackoverflow.com/questions/9512330/multiple-class-attributes-in-html"""
   checks.checkIfString(attributesString, 0, 1000)
   result = []
-  currentAttribute = ""
   idx = 0
   while idx < len(attributesString):
-    currentChar = attributesString[idx]
-    if currentChar.isspace() or currentChar == "=":
-      if currentAttribute and currentChar.isspace():
-        if idx + 1 == len(attributesString) or \
-          (not attributesString[idx + 1].isspace() and attributesString[idx + 1] != "="):
-          if currentAttribute not in result:
-            result.append(currentAttribute)
-          currentAttribute = ""
-        idx += 1
-        continue
-      elif currentAttribute and currentChar == "=":
-        if idx == len(attributesString) - 1:
-          return []
-        nextNonSpaceCharIdx = stringUtil.getFirstNonWhiteSpaceCharIdx(attributesString, idx + 1, len(attributesString))
-        if nextNonSpaceCharIdx == -1:
-          return []
-        nextNonSpaceChar = attributesString[nextNonSpaceCharIdx]
-        if nextNonSpaceChar != "\"" and nextNonSpaceChar != "'":
-          return []
-        quoteCharUsed = nextNonSpaceChar
-        closingQuoteIdx = attributesString.find(quoteCharUsed, nextNonSpaceCharIdx + 1)
-        if closingQuoteIdx == -1:
-          return []
-        if currentAttribute not in result:
-          result.append(currentAttribute)
-        currentAttribute = ""
-        idx = closingQuoteIdx + 1
-        continue
-      elif not currentAttribute and currentChar.isspace():
-        idx += 1
-        continue
-      return []
-    else:
-      currentAttribute += currentChar
-      idx += 1
-      continue
-  if currentAttribute and currentAttribute not in result:
-    result.append(currentAttribute)
+    attributeName, attributeValue, startIdx, endIdx = getNextHtmlAttribute(attributesString, idx)
+    if attributeName is None:
+      nextNonSpaceCharIdx = stringUtil.getFirstNonWhiteSpaceCharIdx(attributesString, idx, len(attributesString))
+      if nextNonSpaceCharIdx != -1:
+        return []
+      break
+    if attributeName not in result:
+      result.append(attributeName)
+    idx = endIdx + 1
+    continue
   return result
