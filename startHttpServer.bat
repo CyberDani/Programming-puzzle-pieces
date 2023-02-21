56485af78b34d3609011ce3c200d459d29026807
Manc Lorant <lorantmanc@gmail.com>
2023-02-21 20:57:23 +0200

    Fix potential bug when running startServer script
    Run python or py depending on which command is recognized by windows.

--- startHttpServer.bat
+++ startHttpServer.bat
@@ -1,1 +1,18 @@
-py -m http.server 9499
+@echo off
+set "command=notfound"
+WHERE py
+IF %ERRORLEVEL% EQU 0 > nul 2>&1 (
+	set "command=py"
+)
+WHERE python
+IF %ERRORLEVEL% EQU 0 > nul 2>&1 (
+	set "command=python"
+)
+IF "%command%" == "notfound" (
+	@echo on
+	echo error:python command not found
+) Else (
+	@echo on
+	echo Start http local server
+	"%command%" -m http.server 9499
+)
